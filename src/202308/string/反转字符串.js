bc00ceeb54024e9e136b1b6422d37f48e610aae9
gm <guimeikevin@gmail.com>
2023-09-20 10:45:42 +0800

    add: 反转字符串

---
+++ src/202308/string/反转字符串.js
@@ -0,0 +1,12 @@
+// 题目：编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。
+// 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+// 思路：使用双指针一个在前，一个在后，每次交换即可。
+
+var reverseString = function (s) {
+  const n = s.length;
+  for (let left = 0, right = n - 1; left < right; left++, right--)
+      [s[left], s[right]] = [s[right], s[left]];
+};
+var s = 'asdfg'
+reverseString(s)
+console.log(s)
