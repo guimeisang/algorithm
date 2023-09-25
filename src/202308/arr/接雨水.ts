716d16c7ee8befd48343f578cc05ea3121764494
gm <guimeikevin@gmail.com>
2023-09-25 11:41:14 +0800

    add: 每天一练算法题，提高下编码速度

--- src/202308/arr/接雨水.ts
+++ src/202308/arr/接雨水.ts
@@ -1,21 +1,62 @@
 // 给定[0,1,0,2,1,0,1,3,2,1,2,1] 
 
 function rain(height) {
-  let res = 0;
-  let left = 0, right = height.length - 1;
-  let leftMax = 0, rightMax = 0;
+  let left = 0, right = height.length - 1
+  let res = 0, leftMax = 0, rightMax = 0
   while(left < right) {
+    leftMax = Math.max(leftMax, height[left])
+    rightMax = Math.max(rightMax, height[right])
     if(height[left] < height[right]) {
-      leftMax = Math.max(leftMax, height[left])
       res += leftMax - height[left]
-      ++left
+      left++
     }else {
-      rightMax = Math.max(rightMax, height[right])
       res += rightMax - height[right]
-      --right
+      right--
     }
   }
   return res
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function rain(height) {
+//   let leftIndex = 0, rightIndex = height.length - 1;
+//   let leftMax = 0, rightMax = 0;
+//   let res = 0
+//   while(leftIndex < rightIndex) {
+//     leftMax = Math.max(leftMax, height[leftIndex])
+//     rightMax = Math.max(rightMax, height[rightIndex])
+//     if(height[leftIndex] < height[rightIndex]) {
+//       res += leftMax - height[leftIndex]
+//       leftIndex++
+//     }else {
+//       res += rightMax - height[rightIndex]
+//       rightIndex--
+//     }
+//   }
+//   return res
+// }
 
 console.log(rain([0,1,0,2,1,0,1,3,2,1,2,1]))
--- src/202308/func/addSumOf.ts
+++ src/202308/func/addSumOf.ts
@@ -3,14 +3,43 @@
 // Add(1,2)(3)(4).sumOf(); // 输出 10  
 // 涉及到的知识面有：闭包、递归、作用域、函数与对象
 function Add() {
-  if(!Add.nums) Add.nums = []
-  Add.nums.push(...arguments)
+  // 只处理参数
+  if(!Add.arr) Add.arr = []
+  console.log('看看参数：', ...arguments)
+  Add.arr.push(...arguments)
   return Add
 }
 
-Add.sumOf = () => {
-  return Add.nums.reduce((a, b) => a + b)
+Add.sumOf = function() {
+  return this.arr.reduce((a, b) => a + b)
 }
 
-console.log(Add(1)(2)(3).sumOf())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Add() {
+//   if(!Add.nums) Add.nums = []
+//   Add.nums.push(...arguments)
+//   return Add
+// }
+
+// Add.sumOf = () => {
+//   return Add.nums.reduce((a, b) => a + b)
+// }
+
+console.log(Add(1,2)(3)(4).sumOf())
 
