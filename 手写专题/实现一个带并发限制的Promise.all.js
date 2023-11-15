4fa7b5507112ea0b2bf7e588062f310dca52f947
gm <guimeikevin@gmail.com>
2023-11-15 16:01:56 +0800

    add：增加2个手写题

--- 手写专题/实现一个带并发限制的Promise.all.js
+++ 手写专题/实现一个带并发限制的Promise.all.js
@@ -4,37 +4,52 @@
  * @param {*} array 待执行的函数列表
  * @param {*} limit 并发限制
  */
-async function asyncPool(array, limit) {
+// async function asyncPool(array, limit) {
+//   // 存储Promise的执行结果
+//   const res = []
+//   // 正在执行的promise
+//   const executing = []
+
+//   for (const item of array) {
+//     const p = item
+//     res.push(p)
+//     if(limit <= array.length) {
+//       p.then(() => executing.splice(0, 1))
+//       executing.push(p)
+//       if(executing.length >= limit) await Promise.race(executing)
+//     }
+//   }
+
+//   return Promise.all(res)
+// }
+
+function asyncPool(poolLimit, array, fn) {
+  let i = 0
   // 存储Promise的执行结果
-  const res = []
+  const ret = []
   // 正在执行的promise
   const executing = []
-
-  for (const item of array) {
-    const p = item
-    res.push(p)
-    if(limit <= array.length) {
-      p.then(() => executing.splice(0, 1))
-      executing.push(p)
-      if(executing.length >= limit) await Promise.race(executing)
-    }
+  // 递归函数
+  const dp = function() {
+    // 边界处理
+    if(array.length === i) return Promise.resolve()
+    // 每次调用dp，初始化一个promise
+    const item = array[i++]
+    const p = Promise.resolve().then(() => fn(item, array))
+    // 放入Promise数组内
+    ret.push(p)
+    const e = p.then(() => executing.splice(executing.indexOf(e), 1))
+    executing.push(e)
+    let r = Promise.resolve()
+    // race 
+    if (executing.length >= poolLimit) r = Promise.race(executing);
+    return r.then(() => dp());
   }
-
-  return Promise.all(res)
+  return dp().then(() => Promise.all(ret))
 }
 
-const timeout = (order, time) => new Promise(resolve => setTimeout(resolve(console.log(order)), time))
+const timeout = (i) => new Promise(resolve => setTimeout(() => resolve(i), i))
 
-const arr = [
-  timeout(1, 100),
-  timeout(2, 200),
-  timeout(3, 300),
-  timeout(4, 400),
-  timeout(5, 500),
-  timeout(6, 500),
-  timeout(7, 400),
-  timeout(8, 300),
-  timeout(9, 200),
-  timeout(10, 100),
-]
-asyncPool(arr, 10).then(result => console.log(result))
+const arr = [100, 200, 300, 400, 500, 600, 700, 800, 900]
+
+asyncPool(5, arr, timeout).then(result => console.log(result))
---
+++ 手写专题/手动实现 Promise.all 方法.js
@@ -0,0 +1,28 @@
+/**
+promise.all的特点如下：
+1、接收一个Promise实例的数组或具有Iterator接口的对象
+2、如果元素不是Promise对象，则使用Promise.resolve转为Promise对象
+3、如果全部成功，状态编委resolved，返回值将组成一个数组传回给回调
+4、只要有一个失败，状态就变成rejected，返回值将直接传递回调all()的返回值也是新的Promise对象
+ * */ 
+
+// 假设已经实现了_Promise
+_Promise.prototype.all = (promiseList) => {
+  return new _Promise((resolve, reject) => {
+    if(!Array.isArray(promiseList)) {
+      reject(new TypeError('参数错误！'))
+    }
+    let count = 0
+    let valueList = new Array(promiseList.length)
+    promiseList.forEach((promise, index) => {
+      _Promise.resolve(promise).then(result => {
+        count++
+        // 每次将返回的结果收集起来
+        valueList[index] = result
+        if(count === promiseList.length) {
+          resolve(valueList)
+        }
+      }, err => reject(err))
+    });
+  })
+}
